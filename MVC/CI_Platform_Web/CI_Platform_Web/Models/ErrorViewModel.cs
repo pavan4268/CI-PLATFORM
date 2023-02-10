fb5fd7e59666a3079b1f801c9ee65de7da7e3d87
pavan4268 <pavangajera333@gmail.com>
2023-02-10 18:11:48 +0530

    CI_Platform_Web is the main file for MVC. Login page created in MVC

---
+++ MVC/CI_Platform_Web/CI_Platform_Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CI_Platform_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
