aab2e400c870e9fd6eaa2cf70e39c06c7987f9d6
pavan4268 <pavangajera333@gmail.com>
2023-02-14 22:57:05 +0530

    Reset Password page Added to MVC

--- MVC/CI_Platform_Web/CI_Platform_Web/Controllers/HomeController.cs
+++ MVC/CI_Platform_Web/CI_Platform_Web/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace CI_Platform_Web.Controllers
             return View();
         }
 
+        public IActionResult reset()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
