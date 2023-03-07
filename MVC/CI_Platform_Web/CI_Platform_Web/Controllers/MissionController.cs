d0616ccc0f9cf355abc53a3237e5b3e121de9b56
Pavan Gajera <PCA221@server7.com>
2023-03-07 10:00:26 +0530

    Implemented dropdown with ajax

--- MVC/CI_Platform_Web/CI_Platform_Web/Controllers/MissionController.cs
+++ MVC/CI_Platform_Web/CI_Platform_Web/Controllers/MissionController.cs
@@ -19,7 +19,7 @@ namespace CI_Platform_Web.Controllers
             return new JsonResult(cnt);
         }
 
-        public JsonResult City(int id)
+        public JsonResult City(long id)
         {
             var cityl = _db.Cities.Where(e => e.CountryId == id).ToList();
             return new JsonResult(cityl);
@@ -46,37 +46,7 @@ namespace CI_Platform_Web.Controllers
 
                
             //}
-            //to get list of the countries
-
-            //var country = _db.Countries.ToList();
-            //var country_list = new SelectList(country, "Countryid", "Name");
-            //ViewBag.countryl = country_list;
-            
-            // to get list of the countries
-
-            //to get list of the cities
-
-            var city = _db.Cities.ToList();
-            var city_list = new SelectList(city, "Cityid", "Name");
-            ViewBag.cityl = city_list;
-            
-            // to get list of the cities
-
-            // to get the list of mission theme
-
-            List<MissionTheme> missionThemes = new List<MissionTheme>();
-            missionThemes = (from a in _db.MissionThemes select a ).ToList();   
-            ViewBag.missionThemes = missionThemes;
-
-            //to get list of mission theme
-
-            // to get list of skills 
-
-            List<MissionSkill> missionSkills = new List<MissionSkill>();
-            missionSkills = (from a in _db.MissionSkills select a ).ToList();
-            ViewBag.missionSkills = missionSkills;
-
-            // to get list of skills
+           
 
             return View();
         }
--- MVC/CI_Platform_Web/CI_Platform_Web/wwwroot/js/Cascade.js
+++ MVC/CI_Platform_Web/CI_Platform_Web/wwwroot/js/Cascade.js
@@ -1,10 +1,19 @@
 ﻿$(document).ready(function () {
     alert('ok');
     GetCountry();
-    //$('#CountryList').change(function () {
-    //    var id = $(this).val();
-    //    $('#City')
-    //});
+    $('#CountryList').change(function () {
+        var id = $(this).val();
+        $('#CityList').empty();
+        $('#CityList').append('<Option>City</Option>');
+        $.ajax({
+            url: '/Mission/City?id=' + id,
+            success: function (result) {
+                $.each(result, function (i, data) {
+                    $('#CityList').append('<Option value =' + data.cityId + '>' + data.name + '</Option>');
+                });
+            }
+        });
+    });
 
 });
 
